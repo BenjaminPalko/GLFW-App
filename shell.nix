76befbfdc8063b71190d025eb5de910cde74dbf5
Benjamin Palko <Benjaminpalko@hotmail.com>
2024-08-03 22:58:07 -0400

    HELLO WORLD!!!

---
+++ shell.nix
@@ -0,0 +1,21 @@
+{
+  pkgs ? import <nixpkgs> { },
+}:
+pkgs.mkShell {
+  # nativeBuildInputs is usually what you want -- tools you need to run
+  nativeBuildInputs = with pkgs.buildPackages; [
+    bear
+    gnumake
+    cmake
+    vcpkg
+    clang
+    clang-tools
+    pkg-config
+  ];
+
+  shellHook = ''
+    export VCPKG_ROOT="${pkgs.vcpkg.outPath}/share/vcpkg";
+    export CC="${pkgs.clang.outPath}/bin/clang";
+    export CXX="${pkgs.clang.outPath}/bin/clang++";
+  '';
+}
