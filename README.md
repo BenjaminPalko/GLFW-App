5c3ef73c8c97935ce557a246035a8c4f1a04e9da
Benjamin Palko <Benjaminpalko@hotmail.com>
2024-08-07 23:55:26 -0400

    and if you do the thing... it just works!

---
+++ README.md
@@ -0,0 +1,13 @@
+# Wayland Application
+
+### Setup
+
+```shell
+mason setup build
+```
+
+### Build
+
+```shell
+mason compile
+```
--- shell.nix
+++ shell.nix
@@ -6,23 +6,20 @@ pkgs.llvmPackages.stdenv.mkDerivation {
   name = "Development";
   buildInputs = with pkgs.buildPackages; [
     fmt
+    fmt.dev
     glfw-wayland
     libGL
     libGL.dev
   ];
   # nativeBuildInputs is usually what you want -- tools you need to run
   nativeBuildInputs = with pkgs.buildPackages; [
+    meson
+    ninja
     gnumake
-    cmake
-    vcpkg
     clang
     clang-tools
     pkg-config
   ];
 
-  shellHook = ''
-    export VCPKG_ROOT="${pkgs.vcpkg.outPath}/share/vcpkg";
-    # export CC="${pkgs.clang.outPath}/bin/clang";
-    # export CXX="${pkgs.clang.outPath}/bin/clang++";
-  '';
+  shellHook = '''';
 }
